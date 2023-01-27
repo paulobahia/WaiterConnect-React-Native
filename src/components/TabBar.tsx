29811223376c24f81f72d03a6738a4de8285df71
Paolo <pauloipatinga22@gmail.com>
2023-01-26 23:13:50 -0300

    Lista de categorias e input de pesquisa

--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -5,30 +5,30 @@ export function TabBar({ navigation }) {
 
     return (
         <>
-            <View className='h-28 bg-white items-center justify-center'>
+            <View className='h-28 bg-slate-50 items-center justify-center'>
                 <View className='bg-slate-900 flex-row w-11/12 rounded-3xl items-center justify-around h-3/5'>
-                    <TouchableOpacity onPress={()=> navigation.navigate('Menu')}>
+                    <TouchableOpacity onPress={() => navigation.navigate('Menu')}>
                         <View className='w-11 h-11 justify-center items-center rounded-full'>
                             <Image
                                 style={{ width: 24, height: 24 }}
                                 source={require('../assets/icons/home.png')} />
                         </View>
                     </TouchableOpacity>
-                    <TouchableOpacity onPress={()=> navigation.navigate('Search')}>
+                    <TouchableOpacity onPress={() => navigation.navigate('Search')}>
                         <View className='w-11 h-11 justify-center items-center rounded-full'>
                             <Image
                                 style={{ width: 24, height: 24 }}
-                                source={require('../assets/icons/search.png')} />
+                                source={require('../assets/icons/time.png')} />
                         </View>
                     </TouchableOpacity>
-                    <TouchableOpacity onPress={()=> navigation.navigate('Tables')}>
+                    <TouchableOpacity onPress={() => navigation.navigate('Tables')}>
                         <View className='w-11 h-11 justify-center items-center rounded-full'>
                             <Image
-                                style={{ width: 30, height: 30 }}
+                                style={{ width: 24, height: 24 }}
                                 source={require('../assets/icons/round-table.png')} />
                         </View>
                     </TouchableOpacity>
-                    <TouchableOpacity onPress={()=> navigation.navigate('Profile')}>
+                    <TouchableOpacity onPress={() => navigation.navigate('Profile')}>
                         <View className='w-11 h-11 justify-center items-center rounded-full'>
                             <Image
                                 style={{ width: 24, height: 24 }}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,83 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, SafeAreaView, Image, ScrollView } from 'react-native';
+import { View, Text, FlatList, Image, TouchableOpacity, StyleSheet, TextInput } from 'react-native';
+import { getCategories } from '../../services';
 
-export function Home(props: any) {
+export function Home() {
+    const [categories, setCategories] = useState(null)
+
+    useEffect(() => {
+        getCategories()
+            .then((response) => setCategories(response.data))
+            .catch((err) => {
+                console.error("ops! ocorrreu um erro" + err);
+            });
+    }, []);
+
+    const CardCategories = ({ data }) =>
+    (
+        <View className='p-5 items-center'>
+            <View style={styles.box} className='bg-white w-14 h-14 justify-center rounded-full'>
+                <TouchableOpacity activeOpacity={0.2}>
+                    <Text className='text-center text-xl'>{data.icon}</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
 
     return (
-        <View className="flex-1 bg-white justify-center">
-            <Text className='text-center'>
-                Home
-            </Text>
+        <View className="flex-1 p-5 bg-slate-50">
+            <View>
+                <View className='flex-row justify-between'>
+                    <View className='flex-row items-center'>
+                        <Image
+                            style={{ width: 50, height: 50 }}
+                            source={require('../../assets/logo.png')} />
+                        <Text className='text-xl font-extrabold'>
+                            Connect Waiter
+                        </Text>
+                    </View>
+                    <TouchableOpacity activeOpacity={0.7}>
+                        <View className='bg-black p-4 items-center rounded-tr-xl rounded-tl-xl rounded-br-3xl rounded-bl-2xl justify-center'>
+                            <Image
+                                style={{ width: 22, height: 22 }}
+                                source={require('../../assets/icons/cart.png')} />
+                        </View>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.box} className='p-3 space-x-3 w-full rounded-full mt-10 bg-white items-center justify-start flex-row'>
+                    <Image
+                        className='ml-2'
+                        style={{ width: 22, height: 22 }}
+                        source={require('../../assets/icons/search.png')} />
+                    <TextInput cursorColor={"#000"} className='flex w-2/3' placeholder="Pesquisar"
+                        underlineColorAndroid="transparent" />
+                </View>
+                <FlatList
+                    className='mt-2'
+                    showsHorizontalScrollIndicator={false}
+                    horizontal
+                    data={categories}
+                    renderItem={({ item }) =>
+                    (
+                        <CardCategories data={item} />
+                    )}
+                    keyExtractor={item => item.id}
+                />
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    box: {
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.23,
+        shadowRadius: 2.62,
+
+        elevation: 4,
+    },
+});
