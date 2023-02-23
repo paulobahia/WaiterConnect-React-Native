09838f8b791e9fdf2c644165b8bdb25e9707e8a7
Bahia <you@example.com>
2023-02-23 02:17:05 -0300

    feat: Finalizado visual componente do carrinho

--- src/screens/Cart/index.tsx
+++ src/screens/Cart/index.tsx
@@ -1,10 +1,14 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { View, Text, TouchableOpacity, FlatList, Image, StyleSheet } from 'react-native';
 import { Ionicons, AntDesign } from '@expo/vector-icons';
+import Lottie from 'lottie-react-native';
+
 
 export function Cart(props) {
     const [listCart, setListCart] = useState([])
     const [totalCart, setTotalCart] = useState(0)
+    const [isScan, setIsScan] = useState(false)
+    const [isQR, setIsQR] = useState(true)
     const { handleCountCart, itensCart } = props.route.params
 
     useEffect(() => {
@@ -49,35 +53,40 @@ export function Cart(props) {
 
     const CartList = ({ data }) => {
         return (
-            <View className='mt-5 shadow-2xl space-x-3 p-4 w-full flex-row bg-white rounded-xl'>
+            <View className='pt-4 w-full items-center flex-row rounded-xl'>
                 <Image
-                    style={{ width: 85, height: 85 }}
+                    style={{ width: 50, height: 50 }}
                     source={{
                         uri: `http://192.168.15.200:3000/uploads/${data.imagePath}`
                     }} />
-                <View className='justify-between'>
-                    <View className='space-y-1'>
-                        <Text className='font-normal text-black text-sm'>{data.name}</Text>
+                <View className='flex-row justify-between items-center w-5/6'>
+                    <View className='w-32'>
+                        <Text numberOfLines={1} className='font-bold ml-3 text-white text-base'>{data.name}</Text>
                     </View>
-                    <View className='items-center w-full space-x-28 flex-row'>
-                        <View className='bg-gray-200 rounded-xl flex items-center w-1/3 justify-center h-10'>
-                            <View className='flex-row items-center space-x-3'>
-                                {data.quantity != 1 ? <TouchableOpacity onPress={() => handlerItemOfCart(data.id, 'decrement')}>
-                                    <AntDesign name="minus" size={20} color='black' />
-                                </TouchableOpacity> : <TouchableOpacity onPress={() => handlerItemOfCart(data.id, 'remove')}>
-                                    <Ionicons name="ios-trash-outline" size={20} color="red" />
-                                </TouchableOpacity>}
-                                <Text className='text-lg font-medium'>
-                                    {data.quantity}
-                                </Text>
-                                <TouchableOpacity onPress={() => handlerItemOfCart(data.id, 'increment')}>
-                                    <AntDesign name="plus" size={20} color="black" />
-                                </TouchableOpacity>
-                            </View>
-                        </View>
-                        <Text className='text-black font-medium text-base'>
+                    <View className=''>
+                        <Text className='text-white font-extralight text-sm'>
                             R$ {data?.price * data.quantity}
                         </Text>
+                    </View>
+                    <View className='flex-row items-center space-x-3'>
+                        {data.quantity > 1 ? <View className='bg-gray-400 p-1 rounded-full'>
+                            <TouchableOpacity onPress={() => handlerItemOfCart(data.id, 'decrement')}>
+                                <AntDesign name="minus" size={12} color='white' />
+                            </TouchableOpacity>
+                        </View> :
+                            <View className='bg-neutral-800 p-1 rounded-full'>
+                                <TouchableOpacity onPress={() => handlerItemOfCart(data.id, 'decrement')}>
+                                    <AntDesign name="minus" size={12} color='gray' />
+                                </TouchableOpacity>
+                            </View>}
+                        <Text className='text-red-400 font-normal text-sm'>
+                            {data.quantity}
+                        </Text>
+                        <View className='bg-gray-400 p-1 rounded-full'>
+                            <TouchableOpacity onPress={() => handlerItemOfCart(data.id, 'increment')}>
+                                <AntDesign name="plus" size={12} color="white" />
+                            </TouchableOpacity>
+                        </View>
                     </View>
                 </View>
             </View>
@@ -96,56 +105,54 @@ export function Cart(props) {
             </View>
             <View className='flex-1'>
                 <View className='flex-1 w-full items-center justify-center bg-neutral-50'>
+                    <View className='flex-row items-center p-2 rounded-md cursor-pointer '>
+                        <TouchableOpacity onPress={() => setIsQR(true)}>
+                            {isQR ? <Text className='px-4 py-2 rounded-l-md shadow-inner bg-gray-700  text-gray-100'>Código QR</Text> :
+                                <Text className='px-4 py-2 rounded-l-md shadow-inner bg-gray-400  text-gray-100'>Código QR</Text>}
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => setIsQR(false)}>
+                            {!isQR ? <Text className='px-4 py-2 rounded-r-md  text-gray-100 bg-gray-700'>Nº da Mesa</Text> :
+                                <Text className='px-4 py-2 rounded-r-md  text-gray-100 bg-gray-400'>Nº da Mesa</Text>}
+                        </TouchableOpacity>
+                    </View>
+                    <TouchableOpacity>
+                        <Lottie
+                            style={{ height: 250 }}
+                            autoPlay={true}
+                            source={require('../../assets/lotties/scan_qrcode.json')}
+                        />
+                    </TouchableOpacity>
                     <Text className='text-gray-700 font-light text-xs'>Aguardando ser escaneado para identificação da mesa</Text>
                 </View>
-                <View className='flex-1 w-full bg-neutral-900'>
-                    <View className='m-6 flex-row items-center justify-between'>
+                <View className='flex-1 w-full p-3 bg-neutral-900'>
+                    <View className='mt-6 flex-row items-center justify-between'>
                         <Text className='text-white text-base font-semibold'>Seu pedido</Text>
-                        <TouchableOpacity className='border-gray-500 border flex-row rounded-md items-center'>
-                            <Text className='text-gray-300 px-2 py-1'>
+                        {isScan ? <TouchableOpacity className='border-gray-500 border flex-row rounded-md items-center'>
+                            <Text className='text-gray-300 font-light text-xs px-2 py-1'>
                                 Enviar pedido
                             </Text>
                             <Ionicons name="arrow-forward" style={{ marginRight: 4 }} size={20} color="white" />
-                        </TouchableOpacity>
+                        </TouchableOpacity> : <TouchableOpacity disabled className='border-gray-600 border flex-row rounded-md items-center'>
+                            <Text className='text-gray-500 font-light text-xs px-2 py-1'>
+                                Enviar pedido
+                            </Text>
+                            <Ionicons name="arrow-forward" style={{ marginRight: 4 }} size={20} color="gray" />
+                        </TouchableOpacity>}
                     </View>
+                    <FlatList
+                        style={{
+                            flexGrow: 0,
+                        }}
+                        showsVerticalScrollIndicator={false}
+                        data={listCart}
+                        renderItem={({ item }) =>
+                        (
+                            <CartList data={item} />
+                        )}
+                        keyExtractor={item => item.id}
+                    />
                 </View>
             </View>
-            {/* <View className='p-5 flex-1 bg-neutral-100'>
-                {listCart.length != 0 ? <FlatList
-                    style={{
-                        flexGrow: 0,
-                    }}
-                    showsVerticalScrollIndicator={false}
-                    data={listCart}
-                    renderItem={({ item }) =>
-                    (
-                        <CartList data={item} />
-                    )}
-                    keyExtractor={item => item.id}
-                />
-                    : null}
-            </View>
-
-            <View className='px-4'>
-                <View className='w-full border-neutral-400 border-b' />
-            </View>
-            <View className='px-10 py-5 bg-neutral-100 w-full'>
-                <View className='flex-row items-center justify-between'>
-                    <Text className='text-xl font-medium'>
-                        Total
-                    </Text>
-                    <Text className='text-xl font-medium'>
-                        R$ {totalCart}
-                    </Text>
-                </View>
-            </View>
-            <View className='items-center bg-neutral-100 p-3 justify-center flex'>
-                <TouchableOpacity className='bg-slate-900 p-4 flex-row w-11/12 rounded-3xl items-center justify-around'>
-                    <Text className='text-lg text-white font-semibold'>
-                        Fazer pedido
-                    </Text>
-                </TouchableOpacity>
-            </View> */}
         </>
     );
 }
